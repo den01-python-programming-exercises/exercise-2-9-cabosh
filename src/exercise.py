23f126392c992c24d04d272ec2010d6a64bbc50f
cabosh <callum.bosher@gmail.com>
2020-06-04 23:07:38 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
 
+    amount = 0
+
+    while True:
+
+        number = int(input("Enter a number: "))
+
+        if number < 0:
+            amount = amount + 1
+        elif number == 0:
+            print(f"Number of negative numbers: {amount}")
+
 if __name__ == '__main__':
     main()
